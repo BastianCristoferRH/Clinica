ff80147e4475fcd5f9fca3fb3df8e40d67302b4c
bastian <bastiancristofer98@gmail.com>
2023-10-07 12:32:16 -0300

    pagina clinica menu

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const navLinks = document.getElementById("nav-links");
 
-    
-    
+    menuIcon.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+
+    // Agregar un evento de clic a las opciones del menú
+    const menuItems = document.querySelectorAll("#nav-links li a");
+    menuItems.forEach((item) => {
+        item.addEventListener("click", function () {
+            navLinks.classList.remove("active");
+        });
+    });
+
+    window.addEventListener("scroll", function () {
+        const sections = document.querySelectorAll("section");
+        const navLinks = document.querySelectorAll("#nav-links li a");
+
+        sections.forEach((section, index) => {
+            const rect = section.getBoundingClientRect();
+
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                navLinks.forEach((link) => {
+                    link.classList.remove("active");
+                });
+                navLinks[index].classList.add("active");
+            }
+        });
+    });
+});
